7b25f12695058aa06b221f21f11a4b1acce40ed4
Andreas Thielmann <thielmann.andreas@googlemail.com>
2023-03-10 11:45:08 +0100

    can extract branch name from git repos

--- domain/mgit.go
+++ domain/mgit.go
@@ -1,5 +1,68 @@
 package mgit
 
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+var (
+	ErrNotARepository = errors.New("Not a git repository")
+)
+
+type gitStatus struct {
+	dirName    string
+	branchName string
+}
+
+func queryGitStatus(baseDirectory string, directory string) (gitStatus, error) {
+	statusCommand := exec.Command("git", "status")
+	statusCommand.Dir = filepath.Join(baseDirectory, directory)
+	output, _ := statusCommand.CombinedOutput()
+	outputString := string(output)
+
+	if strings.Contains(outputString, "fatal: not a git repository") {
+		return gitStatus{}, ErrNotARepository
+	} else {
+		branchName := extractBranchName(outputString)
+		return gitStatus{directory, branchName}, nil
+	}
+}
+
+func extractBranchName(gitStatusOutput string) string {
+	branchRegex := regexp.MustCompile("(?m)On branch (.+)$")
+	branchNames := branchRegex.FindStringSubmatch(gitStatusOutput)
+
+	var branchName string = "unknwon"
+	if len(branchNames) >= 2 {
+		branchName = branchNames[1]
+	}
+	return branchName
+}
+
 func ReportStatus(baseDirectory string) (string, error) {
-	return "git status report", nil
+	dirEntries, err := os.ReadDir(baseDirectory)
+	if err != nil {
+		return "", err
+	}
+
+	var buffer bytes.Buffer
+	var repoIndex int = 0
+
+	for _, entry := range dirEntries {
+		if entry.IsDir() {
+			gitStatus, err := queryGitStatus(baseDirectory, entry.Name())
+			if err == nil {
+				repoIndex++
+				buffer.WriteString(fmt.Sprintf("%02d: %s (%s) [ok]\n", repoIndex, gitStatus.dirName, gitStatus.branchName))
+			}
+		}
+	}
+
+	return buffer.String(), nil
 }
--- domain/mgit_test.go
+++ domain/mgit_test.go
@@ -1,9 +1,12 @@
 package mgit_test
 
 import (
+	mgit "atilm/mgit/domain"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"testing"
 )
 
@@ -18,27 +21,56 @@ func runCommand(command *exec.Cmd) {
 	logError(err)
 }
 
+var testdataDirectory string
+
+func testPath(path string) string {
+	return filepath.Join(testdataDirectory, path)
+}
+
 func TestMain(m *testing.M) {
-	logError(os.MkdirAll("testdata/remote1", os.ModeTemporary))
-	logError(os.MkdirAll("testdata/remote2", os.ModeTemporary))
-	logError(os.MkdirAll("testdata/client1", os.ModeTemporary))
-	logError(os.MkdirAll("testdata/client2", os.ModeTemporary))
-	runCommand(exec.Command("git", "--bare", "init", "testdata/remote1"))
-	runCommand(exec.Command("git", "--bare", "init", "testdata/remote2"))
-	runCommand(exec.Command("git", "--bare", "init", "testdata/remote2"))
-	runCommand(exec.Command("git", "--bare", "init", "testdata/remote2"))
-	runCommand(exec.Command("git", "clone", "testdata/remote1", "testdata/client1/remote1"))
-	runCommand(exec.Command("git", "clone", "testdata/remote2", "testdata/client1/remote2"))
-	runCommand(exec.Command("git", "clone", "testdata/remote1", "testdata/client2/remote1"))
-	runCommand(exec.Command("git", "clone", "testdata/remote2", "testdata/client2/remote2"))
+	var err error
+	testdataDirectory, err = ioutil.TempDir("", "mgit")
+	if err != nil {
+		os.Exit(1)
+	}
+
+	logError(os.MkdirAll(testPath("remote1"), os.ModeTemporary))
+	logError(os.MkdirAll(testPath("remote2"), os.ModeTemporary))
+	logError(os.MkdirAll(testPath("client1"), os.ModeTemporary))
+	logError(os.MkdirAll(testPath("client2"), os.ModeTemporary))
+	runCommand(exec.Command("git", "--bare", "init", testPath("remote1")))
+	runCommand(exec.Command("git", "--bare", "init", testPath("remote2")))
+	runCommand(exec.Command("git", "--bare", "init", testPath("remote2")))
+	runCommand(exec.Command("git", "--bare", "init", testPath("remote2")))
+	runCommand(exec.Command("git", "clone", testPath("remote1"), testPath("client1/remote1")))
+	runCommand(exec.Command("git", "clone", testPath("remote2"), testPath("client1/remote2")))
+	logError(os.MkdirAll(testPath("client1/nonGitDirectory"), os.ModeTemporary))
+	runCommand(exec.Command("git", "clone", testPath("remote1"), testPath("client2/remote1")))
+	runCommand(exec.Command("git", "clone", testPath("remote2"), testPath("client2/remote2")))
 
 	result := m.Run()
 
-	os.RemoveAll("testdata/")
+	os.RemoveAll(testdataDirectory)
 
 	os.Exit(result)
 }
 
-func TestADummy(t *testing.T) {
-	t.Fail()
+func whenTheStatusCommandIsExecuted(baseDirectory string) (string, error) {
+	return mgit.ReportStatus(baseDirectory)
+}
+
+func TestStatusListsAllDirectoriesInGivenBaseDirectory(t *testing.T) {
+	outString, err := whenTheStatusCommandIsExecuted(testPath("client1"))
+
+	expectedResult := `01: remote1 (main) [ok]
+02: remote2 (main) [ok]
+`
+
+	if err != nil {
+		t.Errorf("Expected no error. Got: %v", err)
+	}
+
+	if outString != expectedResult {
+		t.Errorf("Actual: '%v' != Expected: '%v'", outString, expectedResult)
+	}
 }
