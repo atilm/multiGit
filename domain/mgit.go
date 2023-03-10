2971bf4c77decd83f55e7f2b2f1a807223399a2f
Andreas Thielmann <thielmann.andreas@googlemail.com>
2023-03-10 13:00:59 +0100

    can count remote commits

--- domain/mgit.go
+++ domain/mgit.go
@@ -8,6 +8,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -16,21 +17,32 @@ var (
 )
 
 type gitStatus struct {
-	dirName    string
-	branchName string
+	dirName       string
+	branchName    string
+	commitsToPull int
+	commitsToPush int
 }
 
 func queryGitStatus(baseDirectory string, directory string) (gitStatus, error) {
+	fullRepoPath := filepath.Join(baseDirectory, directory)
+
+	fetchCommand := exec.Command("git", "fetch")
+	fetchCommand.Dir = fullRepoPath
+	fetchCommand.Run()
+
 	statusCommand := exec.Command("git", "status")
-	statusCommand.Dir = filepath.Join(baseDirectory, directory)
+	statusCommand.Dir = fullRepoPath
 	output, _ := statusCommand.CombinedOutput()
 	outputString := string(output)
+
+	// fmt.Print(outputString)
 
 	if strings.Contains(outputString, "fatal: not a git repository") {
 		return gitStatus{}, ErrNotARepository
 	} else {
 		branchName := extractBranchName(outputString)
-		return gitStatus{directory, branchName}, nil
+		commitsToPull, commitsToPush := extractChanges(outputString)
+		return gitStatus{directory, branchName, commitsToPull, commitsToPush}, nil
 	}
 }
 
@@ -43,6 +55,22 @@ func extractBranchName(gitStatusOutput string) string {
 		branchName = branchNames[1]
 	}
 	return branchName
+}
+
+func extractChanges(gitStatusOutput string) (int, int) {
+	behindRegex := regexp.MustCompile(`Your branch is behind .+ by (\d+) commit`)
+	numbers := behindRegex.FindStringSubmatch(gitStatusOutput)
+
+	if len(numbers) >= 2 {
+		commitsToPull, err := strconv.Atoi(numbers[1])
+		if err != nil {
+			return 0, 0
+		} else {
+			return commitsToPull, 0
+		}
+	} else {
+		return 0, 0
+	}
 }
 
 func ReportStatus(baseDirectory string) (string, error) {
@@ -59,7 +87,12 @@ func ReportStatus(baseDirectory string) (string, error) {
 			gitStatus, err := queryGitStatus(baseDirectory, entry.Name())
 			if err == nil {
 				repoIndex++
-				buffer.WriteString(fmt.Sprintf("%02d: %s (%s) [ok]\n", repoIndex, gitStatus.dirName, gitStatus.branchName))
+				if gitStatus.commitsToPull == 0 && gitStatus.commitsToPush == 0 {
+					buffer.WriteString(fmt.Sprintf("%02d: %s (%s) [ok]\n", repoIndex, gitStatus.dirName, gitStatus.branchName))
+				} else {
+					buffer.WriteString(fmt.Sprintf("%02d: %s (%s) [%d to pull, %d to push]\n", repoIndex, gitStatus.dirName, gitStatus.branchName,
+						gitStatus.commitsToPull, gitStatus.commitsToPush))
+				}
 			}
 		}
 	}
--- domain/mgit_test.go
+++ domain/mgit_test.go
@@ -17,14 +17,30 @@ func logError(err error) {
 }
 
 func runCommand(command *exec.Cmd) {
-	err := command.Run()
+	output, err := command.CombinedOutput()
+
+	if err != nil {
+		fmt.Print(string(output))
+	}
+
 	logError(err)
+}
+
+func runCommandInDir(command *exec.Cmd, directory string) {
+	command.Dir = directory
+	runCommand(command)
 }
 
 var testdataDirectory string
 
 func testPath(path string) string {
 	return filepath.Join(testdataDirectory, path)
+}
+
+func createFile(filePath string) error {
+	file, err := os.Create(filePath)
+	file.Close()
+	return err
 }
 
 func TestMain(m *testing.M) {
@@ -34,19 +50,37 @@ func TestMain(m *testing.M) {
 		os.Exit(1)
 	}
 
+	fmt.Println(testdataDirectory)
+
+	// init bare "remote" repositories
 	logError(os.MkdirAll(testPath("remote1"), os.ModeTemporary))
 	logError(os.MkdirAll(testPath("remote2"), os.ModeTemporary))
-	logError(os.MkdirAll(testPath("client1"), os.ModeTemporary))
-	logError(os.MkdirAll(testPath("client2"), os.ModeTemporary))
 	runCommand(exec.Command("git", "--bare", "init", testPath("remote1")))
 	runCommand(exec.Command("git", "--bare", "init", testPath("remote2")))
-	runCommand(exec.Command("git", "--bare", "init", testPath("remote2")))
-	runCommand(exec.Command("git", "--bare", "init", testPath("remote2")))
+
+	// create a writing client
+	// perhaps push a first commit in client 2 remotes before cloning into client 1
+	logError(os.MkdirAll(testPath("client2"), os.ModeTemporary))
+	runCommand(exec.Command("git", "clone", testPath("remote1"), testPath("client2/remote1")))
+	runCommand(exec.Command("git", "clone", testPath("remote2"), testPath("client2/remote2")))
+
+	// initial commit
+	logError(createFile(testPath("client2/remote2/file1.txt")))
+	runCommandInDir(exec.Command("git", "add", "."), testPath("client2/remote2"))
+	runCommandInDir(exec.Command("git", "commit", "-m", "\"message 1\""), testPath("client2/remote2"))
+	runCommandInDir(exec.Command("git", "push"), testPath("client2/remote2"))
+
+	// create a reading client
+	logError(os.MkdirAll(testPath("client1"), os.ModeTemporary))
 	runCommand(exec.Command("git", "clone", testPath("remote1"), testPath("client1/remote1")))
 	runCommand(exec.Command("git", "clone", testPath("remote2"), testPath("client1/remote2")))
 	logError(os.MkdirAll(testPath("client1/nonGitDirectory"), os.ModeTemporary))
-	runCommand(exec.Command("git", "clone", testPath("remote1"), testPath("client2/remote1")))
-	runCommand(exec.Command("git", "clone", testPath("remote2"), testPath("client2/remote2")))
+
+	// commit not in client1 copies
+	logError(createFile(testPath("client2/remote2/file2.txt")))
+	runCommandInDir(exec.Command("git", "add", "."), testPath("client2/remote2"))
+	runCommandInDir(exec.Command("git", "commit", "-m", "\"message 1\""), testPath("client2/remote2"))
+	runCommandInDir(exec.Command("git", "push"), testPath("client2/remote2"))
 
 	result := m.Run()
 
@@ -63,7 +97,7 @@ func TestStatusListsAllDirectoriesInGivenBaseDirectory(t *testing.T) {
 	outString, err := whenTheStatusCommandIsExecuted(testPath("client1"))
 
 	expectedResult := `01: remote1 (main) [ok]
-02: remote2 (main) [ok]
+02: remote2 (main) [1 to pull, 0 to push]
 `
 
 	if err != nil {
